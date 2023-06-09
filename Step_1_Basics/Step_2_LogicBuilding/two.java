4f407bab17d5eea2843d8669abf5a2721039fb31
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-09 08:51:14 +0530

    two

---
+++ Step_1_Basics/Step_2_LogicBuilding/two.java
@@ -0,0 +1,20 @@
+package Step_1_Basics.Step_2_LogicBuilding;
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * *
+
+public class two {
+    public static void main(String[] args) {
+        System.out.println();
+        for(int i=0; i<5; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
