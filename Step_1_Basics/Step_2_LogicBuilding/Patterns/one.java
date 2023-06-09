0d0bbf9754f3220b4ad9f092ea1d474545efec89
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-09 08:34:09 +0530

    one

--- Step_1_Basics/Step_2_LogicBuilding/Patterns/one.java
+++
@@ -1,5 +0,0 @@
-package Step_1_Basics.Step_2_LogicBuilding.Patterns;
-
-public class one {
-    
-}
---
+++ Step_1_Basics/Step_2_LogicBuilding/one.java
@@ -0,0 +1,14 @@
+package Step_1_Basics.Step_2_LogicBuilding;
+
+public class one {
+    public static void main(String[] args) {
+        System.out.println();
+        for(int i=0; i<5; i++){
+            for(int j=0; j<5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
