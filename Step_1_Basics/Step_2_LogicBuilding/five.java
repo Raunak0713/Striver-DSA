5e43856ab79946eb9e1fa6cb7edaaeeeba7b4ad4
Raunak0713 <raunakbhanarkar7@gmail.com>
2023-06-09 09:13:57 +0530

    five

---
+++ Step_1_Basics/Step_2_LogicBuilding/five.java
@@ -0,0 +1,20 @@
+package Step_1_Basics.Step_2_LogicBuilding;
+
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+public class five {
+    public static void main(String[] args) {
+        System.out.println();
+        for(int i=0; i<5; i++){
+            for(int j=(5-i); j>0; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
