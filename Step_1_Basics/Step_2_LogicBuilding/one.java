a914b02aa95478e514acaf6fa6cab671b6c48c06
Raunak0713 <100823919+Raunak0713@users.noreply.github.com>
2023-06-09 08:51:37 +0530

    Update one.java

--- Step_1_Basics/Step_2_LogicBuilding/one.java
+++ Step_1_Basics/Step_2_LogicBuilding/one.java
@@ -11,7 +11,7 @@ public class one {
         System.out.println();
         for(int i=0; i<5; i++){
             for(int j=0; j<5; j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
